ec29e232d83d520a3919c44745ce64bbe04544dc
cedarwarman <warmanma@oregonstate.edu>
2020-04-18 11:30:28 -0700

    Lots of changes

--- R/confidence_threshold_tests.R
+++ R/confidence_threshold_tests.R
@@ -1,130 +1,206 @@
 ###### Confidence threshold tests ######
 # Here we empirically determine the optimal confidence threshold for bounding 
-# bounding box outputs (Supplemental Figure 2)
+# bounding box outputs, based on the test image set (Supplemental Figure 2).
 
 library(tidyverse)
 
 ###### Importing the data ######
 # Hand counted validation data
-hand_validations <- read.table(file="./data/test_set_hand_validations.tsv",
+hand_validations <- read.table(file="./data/hand_counts.tsv",
                                sep = '\t',
                                header = TRUE)
 hand_validations$image_name <- as.character(hand_validations$image_name)
-### ADD X TO THE NAMES HERE, THEN DON'T DELETE IT ON THE MODEL PREDICTIONS
 
-# Model predictions
-model_predictions <- read.table(file="./data/test_set_model_predictions.tsv",
+# 2018 model predictions
+model_predictions_2018 <- read.table(file="./data/test_set_two_models_predictions_2018_detailed.tsv",
                                 sep = '\t',
                                 header = TRUE)
 ### CHANGE THIS ON THE INPUT TSV FILE THEN DELETE THIS STEP
-colnames(model_predictions)[7] <- "image_name"
-model_predictions$image_name <- as.character(model_predictions$image_name)
-### CHANGE THIS ON THE INPUT TSV FILE THEN DELETE THIS STEP
-model_predictions$image_name <- str_remove(model_predictions$image_name, 'X')
+colnames(model_predictions_2018)[7] <- "image_name"
+model_predictions_2018$image_name <- as.character(model_predictions_2018$image_name)
 
 # There's some rows with zeros (probably resulting from some bad code 
 # when exporting the data)
 ### DELETE THESE IN FINAL SET
-model_predictions <- model_predictions[model_predictions$score != 0, ]
+model_predictions_2018 <- model_predictions_2018[model_predictions_2018$score != 0, ]
+
+
+# 2019 model predictions
+model_predictions_2019 <- read.table(file="./data/test_set_two_models_predictions_2019_detailed.tsv",
+                                     sep = '\t',
+                                     header = TRUE)
+### CHANGE THIS ON THE INPUT TSV FILE THEN DELETE THIS STEP
+colnames(model_predictions_2019)[7] <- "image_name"
+model_predictions_2019$image_name <- as.character(model_predictions_2019$image_name)
+
+# There's some rows with zeros (probably resulting from some bad code 
+# when exporting the data)
+### DELETE THESE IN FINAL SET
+model_predictions_2019 <- model_predictions_2019[model_predictions_2019$score != 0, ]
 
 
 ###### Summarizing fluorescent/non-fluorescent R-squared values at different confidence thresholds ######
-### Fluorescent kernels
-get_fluor_r_squared <- function(threshold, input_df, hand_counts) {
-  df <- input_df %>% 
-    group_by(image_name, class) %>%
-    filter(score > threshold) %>%
-    filter(class == 1) %>%
-    summarize(count = n())
-  
-  df <- inner_join(df, hand_counts)
-  df <- df[complete.cases(df), ]
-  
-  # Returns 0 when there are no bounding boxes that satisfy a threshold
-  if (nrow(df) == 0) {
-    output <- data.frame("threshold" = threshold, "r_squared" = 0)
-    return(output)
+### Function for fluorescent kernels
+get_fluor_r_squared <- function(input_df, hand_counts) {
+  # Sub function that calculates the R-squared for a single threshold
+  get_single_fluor_r_squared <- function(threshold, input_df, hand_counts) {
+    df <- input_df %>% 
+      group_by(image_name, class) %>%
+      filter(score > threshold) %>%
+      filter(class == 1) %>%
+      summarize(count = n())
     
-  # Runs the regression if bounding boxes are present
-  } else {
-    regression_r <- summary(lm(count ~ GFP_hand, data = df))$adj.r.squared
-    output <- data.frame("threshold" = threshold, "r_squared" = regression_r)
-    return(output)
-  }
-}
-
-# This sets up a list, which will become a dataframe after it's populated by 
-# the following for loop
-df_list_fluor <- list()
-i = 1
-for (x in seq(0.01, 1, by=0.01)) {
-  print(x)
-  output <- get_fluor_r_squared(x, model_predictions, hand_validations)
-  df_list_fluor[[i]] <- output
-  i = i + 1
-}
-
-fluor_r_squared <- bind_rows(df_list_fluor)
-
-### Non-fluorescent kernels
-get_nonfluor_r_squared <- function(threshold, input_df, hand_counts) {
-  df <- input_df %>% 
-    group_by(image_name, class) %>%
-    filter(score > threshold) %>%
-    filter(class == 2) %>%
-    summarize(count = n())
-  
-  df <- inner_join(df, hand_counts)
-  df <- df[complete.cases(df), ]
-  
-  # Returns 0 when there are no bounding boxes that satisfy a threshold
-  if (nrow(df) == 0) {
-    output <- data.frame("threshold" = threshold, "r_squared" = 0)
-    return(output)
+    df <- inner_join(df, hand_counts)
+    df <- df[complete.cases(df), ]
     
-    # Runs the regression if bounding boxes are present
-  } else {
-    regression_r <- summary(lm(count ~ wt_hand, data = df))$adj.r.squared
-    output <- data.frame("threshold" = threshold, "r_squared" = regression_r)
-    return(output)
+    # Returns 0 when there are no bounding boxes that satisfy a threshold
+    if (nrow(df) == 0) {
+      output <- data.frame("threshold" = threshold, "r_squared" = 0)
+      return(output)
+      
+      # Runs the regression if bounding boxes are present
+    } else {
+      regression_r <- summary(lm(count ~ GFP_hand, data = df))$adj.r.squared
+      output <- data.frame("threshold" = threshold, "r_squared" = regression_r)
+      return(output)
+    }
   }
+  
+  # This sets up a list, which will become a dataframe after it's populated by 
+  # the following for loop
+  df_list_fluor <- list()
+  i = 1
+  for (x in seq(0.01, 1, by=0.01)) {
+    print(x)
+    output <- get_single_fluor_r_squared(x, input_df, hand_counts)
+    df_list_fluor[[i]] <- output
+    i = i + 1
+  }
+  
+  fluor_r_squared <- bind_rows(df_list_fluor)
+  return(fluor_r_squared)
 }
 
-# This sets up a list, which will become a dataframe after it's populated by 
-# the following for loop
-df_list_nonfluor <- list()
-i = 1
-for (x in seq(0.01, 1, by=0.01)) {
-  print(x)
-  output <- get_nonfluor_r_squared(x, model_predictions, hand_validations)
-  df_list_nonfluor[[i]] <- output
-  i = i + 1
+### Function for nonluorescent kernels
+get_nonfluor_r_squared <- function(input_df, hand_counts) {
+  # Sub function that calculates the R-squared for a single threshold
+  get_single_nonfluor_r_squared <- function(threshold, input_df, hand_counts) {
+    df <- input_df %>% 
+      group_by(image_name, class) %>%
+      filter(score > threshold) %>%
+      filter(class == 2) %>%
+      summarize(count = n())
+    
+    df <- inner_join(df, hand_counts)
+    df <- df[complete.cases(df), ]
+    
+    # Returns 0 when there are no bounding boxes that satisfy a threshold
+    if (nrow(df) == 0) {
+      output <- data.frame("threshold" = threshold, "r_squared" = 0)
+      return(output)
+      
+      # Runs the regression if bounding boxes are present
+    } else {
+      regression_r <- summary(lm(count ~ wt_hand, data = df))$adj.r.squared
+      output <- data.frame("threshold" = threshold, "r_squared" = regression_r)
+      return(output)
+    }
+  }
+  
+  # This sets up a list, which will become a dataframe after it's populated by 
+  # the following for loop
+  df_list_nonfluor <- list()
+  i = 1
+  for (x in seq(0.01, 1, by=0.01)) {
+    print(x)
+    output <- get_single_nonfluor_r_squared(x, input_df, hand_counts)
+    df_list_nonfluor[[i]] <- output
+    i = i + 1
+  }
+  
+  nonfluor_r_squared <- bind_rows(df_list_nonfluor)
+  return(nonfluor_r_squared)
 }
 
-nonfluor_r_squared <- bind_rows(df_list_nonfluor)
+### Calculating fluorescent R-squared values
+fluor_r_squared_2018 <- get_fluor_r_squared(model_predictions_2018, hand_validations)
+fluor_r_squared_2019 <- get_fluor_r_squared(model_predictions_2019, hand_validations)
+
+### Calculating nonfluorescent R-squared values
+nonfluor_r_squared_2018 <- get_nonfluor_r_squared(model_predictions_2018, hand_validations)
+nonfluor_r_squared_2019 <- get_nonfluor_r_squared(model_predictions_2019, hand_validations)
 
 
-###### Calculating the optimal confidence threshold ######
+###### Calculating the optimal confidence threshold 2019 ######
 # I will define the optimal confidence threshold as the confidence level that 
 # maximizes the combined fluorescent and non-fluorescent R-squared.
-joined_df <- data.frame(threshold = fluor_r_squared$threshold, 
-                        fluor_r_squared = fluor_r_squared$r_squared, 
-                        nonfluor_r_squared = nonfluor_r_squared$r_squared)
-joined_df$sum_r_squared <- joined_df$fluor_r_squared + joined_df$nonfluor_r_squared
-max_r_squared <- joined_df$threshold[joined_df$sum_r_squared == max(joined_df$sum_r_squared)]
+
+joined_df_2018 <- data.frame(threshold = fluor_r_squared_2018$threshold,
+                             fluor_r_squared = fluor_r_squared_2018$r_squared,
+                             nonfluor_r_squared = nonfluor_r_squared_2018$r_squared)
+joined_df_2018$sum_r_squared <- joined_df_2018$fluor_r_squared + joined_df_2018$nonfluor_r_squared
+joined_df_2018 <- joined_df_2018[complete.cases(joined_df_2018), ]
+max_r_squared_2018 <- joined_df_2018$threshold[joined_df_2018$sum_r_squared == max(joined_df_2018$sum_r_squared)]
+
+joined_df_2019 <- data.frame(threshold = fluor_r_squared_2019$threshold,
+                             fluor_r_squared = fluor_r_squared_2019$r_squared,
+                             nonfluor_r_squared = nonfluor_r_squared_2019$r_squared)
+joined_df_2019$sum_r_squared <- joined_df_2019$fluor_r_squared + joined_df_2019$nonfluor_r_squared
+joined_df_2019 <- joined_df_2019[complete.cases(joined_df_2019), ]
+max_r_squared_2019 <- joined_df_2019$threshold[joined_df_2019$sum_r_squared == max(joined_df_2019$sum_r_squared)]
+
 
 ###### Plotting (Supplemental Figure 2) ######
-# Making the data tidy
-to_be_tidied <- joined_df[ , 1:3]
-colnames(to_be_tidied) <- c("Threshold", "Fluorescent", "Non-fluorescent")
-tidy_joined_df <- gather(to_be_tidied, "Class", "R_squared", 2:3)
+# Function to make the data tidy for plotting
+tidy_data_for_plot <- function(input_df) {
+  to_be_tidied <- input_df[ , 1:3]
+  colnames(to_be_tidied) <- c("Threshold", "Fluorescent", "Non-fluorescent")
+  tidy_joined_df <- gather(to_be_tidied, "Class", "R_squared", 2:3)
+  return(tidy_joined_df)
+}
 
-ggplot(tidy_joined_df, aes(x = Threshold, y = R_squared, group = Class)) +
+# Plotting 2018 data
+plot_2018 <- tidy_data_for_plot(joined_df_2018)
+
+ggplot(plot_2018, aes(x = Threshold, y = R_squared, group = Class)) +
   geom_point(aes(shape = Class), size = 2) +
   scale_shape_manual(values = c(19, 4)) +
-  geom_vline(xintercept = max_r_squared, linetype = 'dashed', size = 1) +
+  geom_vline(xintercept = max_r_squared_2018, linetype = 'dashed', size = 1) +
   coord_fixed(xlim = c(0, 1), ylim = c(0, 1)) +
-  labs(title = 'Optimal confidence threshold', x = 'Threshold', y = 'Adj. R-squared') +
+  labs(title = 'Optimal confidence threshold 2018', x = 'Threshold', y = 'Adj. R-squared') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 24, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 28, face = 'bold', margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.5, 0.5, 0.5, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/optimal_confidence_thresholds_2018.png',
+       device = 'png',
+       width = 9,
+       height = 8,
+       dpi = 400,
+       units = 'in')
+
+# Plotting 2019 data
+plot_2019 <- tidy_data_for_plot(joined_df_2019)
+
+ggplot(plot_2019, aes(x = Threshold, y = R_squared, group = Class)) +
+  geom_point(aes(shape = Class), size = 2) +
+  scale_shape_manual(values = c(19, 4)) +
+  geom_vline(xintercept = max_r_squared_2019, linetype = 'dashed', size = 1) +
+  coord_fixed(xlim = c(0, 1), ylim = c(0, 1)) +
+  labs(title = 'Optimal confidence threshold 2019', x = 'Threshold', y = 'Adj. R-squared') +
   theme_bw() +
   theme(axis.title = element_text(size = 24, face = 'bold'),
         axis.text = element_text(size = 18, face = 'bold'),
@@ -144,7 +220,7 @@ ggplot(tidy_joined_df, aes(x = Threshold, y = R_squared, group = Class)) +
         legend.text = element_text(size = 16, face = 'bold'),
         legend.title = element_text(size = 18, face = 'bold'))
 
-ggsave(filename = './plots/optimal_confidence_thresholds.png',
+ggsave(filename = './plots/optimal_confidence_thresholds_2019.png',
        device = 'png',
        width = 10,
        height = 8,
---
+++ R/deep_learning_individual_models_test_set.R
@@ -0,0 +1,258 @@
+###### Deep learning model test set ######
+# Here we test the effectiveness of our deep learning model on 2018 and 2019 
+# field season test sets, based on seperate models trained on images from each
+# year. For validation, we used hand-counted total kernel numbers.
+
+library(tidyverse)
+
+
+###### Importing the data ######
+
+# Hand-counted validation data
+hand_counted <- read.table(file="./data/hand_counts.tsv",
+                           sep = '\t',
+                           header = TRUE)
+hand_counted$image_name <- as.character(hand_counted$image_name)
+
+# Function to import and tidy the data
+import_tensorflow_summary <- function(import_path, hand_counted_df) {
+  library(tidyverse)
+  tensorflow_output <- read.table(file=import_path,
+                                  sep = '\t',
+                                  header = FALSE)
+  colnames(tensorflow_output) <- c("image_name", "GFP_tf", "wt_tf")
+  tensorflow_output$image_name <- as.character(tensorflow_output$image_name)
+  
+  df <- full_join(hand_counted_df, tensorflow_output)
+  df <- df[complete.cases(df), ]
+  
+  df <- df %>% mutate(percent_GFP_hand = GFP_hand / (GFP_hand + wt_hand),
+                      percent_GFP_tf = GFP_tf / (GFP_tf + wt_tf))
+  
+  return(df)
+}
+
+results_2018 <- import_tensorflow_summary("./data/test_set_two_models_predictions_2018_summary.tsv", hand_counted)
+results_2019 <- import_tensorflow_summary("./data/test_set_two_models_predictions_2019_summary.tsv", hand_counted)
+
+
+###### Plotting the results (Figure 5B) ######
+### 2018 Fluorescent
+ggplot(results_2018, aes(x = GFP_hand, y = GFP_tf)) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 1, color = '#4D4D4D') +
+  geom_point(alpha = 1, size = 2, shape = 21, stroke = 1, color = 'black', fill = '#98f542') +
+  coord_fixed(xlim = c(0, 350), ylim = c(0, 350)) +
+  labs(title = 'Fluorescent kernels 2018', x = 'Manual count', y = 'Tensorflow') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 20, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 22, face = 'bold', hjust = 0.5, margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.8, 0.5, 0.3, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/tensorflow_GFP_2018.png', 
+       device = 'png', 
+       width = 5,
+       height = 5, 
+       dpi = 400,
+       units = 'in')
+
+### 2018 Nonfluorescent
+ggplot(results_2018, aes(x = wt_hand, y = wt_tf)) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 1, color = '#4D4D4D') +
+  geom_point(alpha = 1, size = 2, shape = 21, stroke = 1, color = 'black', fill = '#614973') +
+  coord_fixed(xlim = c(0, 350), ylim = c(0, 350)) +
+  labs(title = 'Nonfluorescent kernels 2018', x = 'Manual count', y = 'Tensorflow') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 20, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 22, face = 'bold', hjust = 0.5, margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.8, 0.5, 0.3, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/tensorflow_wt_2018.png', 
+       device = 'png', 
+       width = 5,
+       height = 5, 
+       dpi = 400,
+       units = 'in')
+
+### 2018 Percent GFP
+ggplot(results_2018, aes(x = 100 * percent_GFP_hand, y = 100 * percent_GFP_tf)) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 1, color = '#4D4D4D') +
+  geom_point(alpha = 1, size = 2, shape = 21, stroke = 1, color = 'black', fill = 'orange') +
+  coord_fixed(xlim = c(0, 62), ylim = c(0, 62)) +
+  labs(title = 'Percent fluorescent 2018', x = 'Manual count', y = 'Tensorflow') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 20, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 22, face = 'bold', hjust = 0.5, margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.8, 0.5, 0.3, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/tensorflow_percent_2018.png', 
+       device = 'png', 
+       width = 5,
+       height = 5, 
+       dpi = 400,
+       units = 'in')
+
+### 2019 Fluorescent
+ggplot(results_2019, aes(x = GFP_hand, y = GFP_tf)) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 1, color = '#4D4D4D') +
+  geom_point(alpha = 1, size = 2, shape = 21, stroke = 1, color = 'black', fill = '#98f542') +
+  coord_fixed(xlim = c(0, 350), ylim = c(0, 350)) +
+  labs(title = 'Fluorescent kernels 2019', x = 'Manual count', y = 'Tensorflow') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 20, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 22, face = 'bold', hjust = 0.5, margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.8, 0.5, 0.3, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/tensorflow_GFP_2019.png', 
+       device = 'png', 
+       width = 5,
+       height = 5, 
+       dpi = 400,
+       units = 'in')
+
+### 2019 Nonfluorescent
+ggplot(results_2019, aes(x = wt_hand, y = wt_tf)) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 1, color = '#4D4D4D') +
+  geom_point(alpha = 1, size = 2, shape = 21, stroke = 1, color = 'black', fill = '#614973') +
+  coord_fixed(xlim = c(0, 350), ylim = c(0, 350)) +
+  labs(title = 'Nonfluorescent kernels 2019', x = 'Manual count', y = 'Tensorflow') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 20, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 22, face = 'bold', hjust = 0.5, margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.8, 0.5, 0.3, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/tensorflow_wt_2019.png', 
+       device = 'png', 
+       width = 5,
+       height = 5, 
+       dpi = 400,
+       units = 'in')
+
+### 2019 Percent GFP
+ggplot(results_2019, aes(x = 100 * percent_GFP_hand, y = 100 * percent_GFP_tf)) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 1, color = '#4D4D4D') +
+  geom_point(alpha = 1, size = 2, shape = 21, stroke = 1, color = 'black', fill = 'orange') +
+  coord_fixed(xlim = c(0, 62), ylim = c(0, 62)) +
+  labs(title = 'Percent fluorescent 2019', x = 'Manual count', y = 'Tensorflow') +
+  theme_bw() +
+  theme(axis.title = element_text(size = 20, face = 'bold'),
+        axis.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(size = 22, face = 'bold', hjust = 0.5, margin = margin(0, 0, 10, 0)),
+        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        axis.line = element_line(size = 0, color = '#4D4D4D'),
+        axis.ticks = element_line(size = 0.75, color = '#4D4D4D'),
+        axis.ticks.length = unit(4, 'pt'),
+        plot.margin = margin(0.5, 0.8, 0.5, 0.3, 'cm'),
+        panel.border = element_rect(color = '#4D4D4D', size = 2, fill = NA),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.5),
+        panel.grid.major.x = element_line(size = 0.75),
+        panel.grid.minor.x = element_line(size = 0.5),
+        legend.position = 'none')
+
+ggsave(filename = './plots/tensorflow_percent_2019.png', 
+       device = 'png', 
+       width = 5,
+       height = 5, 
+       dpi = 400,
+       units = 'in')
+
+
+###### Linear regression ######
+do_stats <- function(name, input_df) {
+  fluorescent_lm <- lm(GFP_hand ~ GFP_tf, data = input_df)
+  fluorescent_r_squared <- summary(fluorescent_lm)$adj.r.squared
+  
+  nonfluorescent_lm <- lm(wt_hand ~ wt_tf, data = input_df)
+  nonfluorescent_r_squared <- summary(nonfluorescent_lm)$adj.r.squared
+  
+  percent_GFP_lm <- lm(percent_GFP_hand ~ percent_GFP_tf, data = input_df)
+  percent_GFP_r_squared <- summary(percent_GFP_lm)$adj.r.squared
+  
+  output_df <- data.frame(name = rep(name, 3),
+                          r_squared_category = c("fluorescent", "nonfluorescent", "percent_GFP"),
+                          r_squared = c(fluorescent_r_squared, nonfluorescent_r_squared, percent_GFP_r_squared))
+  
+  return(output_df)
+}
+
+stats_results_2018 <- do_stats("Tensorflow 2018", results_2018)
+stats_results_2019 <- do_stats("Tensorflow 2019", results_2019)
+
+stats <- rbind(stats_results_2018, stats_results_2019)
+
+
+###### Mean absolute deviation ######
+# Getting the mean number of kernels per ear
+mean_total_kernels <- mean(c(results_2018$GFP_hand, results_2019$GFP_hand) +
+                             c(results_2018$wt_hand, results_2019$wt_hand))
+
+# Mean absolute deviations 2018
+mean_abs_dev_fluorescent_2018 <- mean(abs(results_2018$GFP_hand - results_2018$GFP_tf))
+mean_abs_dev_wt_2018 <- mean(abs(results_2018$wt_hand - results_2018$wt_tf))
+mean_abs_dev_percent_2018 <- mean(abs(results_2018$percent_GFP_hand - results_2018$percent_GFP_tf))
+
+# Mean absolute deviations 2019
+mean_abs_dev_fluorescent_2019 <- mean(abs(results_2019$GFP_hand - results_2019$GFP_tf))
+mean_abs_dev_wt_2019 <- mean(abs(results_2019$wt_hand - results_2019$wt_tf))
+mean_abs_dev_percent_2019 <- mean(abs(results_2019$percent_GFP_hand - results_2019$percent_GFP_tf))
